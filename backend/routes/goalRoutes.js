9fa0f99b4e812fd7c9aa79d814ef6de61f968bf6
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-04-15 16:24:30 -0500

    goals controller and routes set up

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+const {getGoals, setGoal, updateGoal, deleteGoal
+} = require('../controllers/goalController')
+
+// router.get('/', getGoals)
+
+// router.post('/',setGoal)
+
+router.route('/').get(getGoals).post(setGoal)
+
+// router.put('/:id',updateGoal)
+
+// router.delete('/:id',deleteGoal)
+
+router.route('/:id').delete(deleteGoal).put(updateGoal)
+
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("Hellow World")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require ('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, ()=> console.log(`Listening on port ${port}`))
