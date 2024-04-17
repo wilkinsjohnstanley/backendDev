75ca82ba3c15812422f38ee241e88ecb552aed87
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-04-17 14:44:05 -0500

    Backend CRUD is DONE!!!!

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require('express-async-handler')
 
 const Goal = require('../model/goalModel')
+const User = require('../model/userModel')
+
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
 const getGoals = asyncHandler(async(req, res)=>{
-    const goals = await Goal.find()
+    const goals = await Goal.find({user:req.user.id})
     res.status(200).json(goals)
 })
 // @desc Set Goal
@@ -17,7 +19,8 @@ const setGoal = asyncHandler(async(req, res)=>{
         throw new Error('Please add a text field')
     }
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
     res.status(200).json(goal);
 })
@@ -29,6 +32,18 @@ const updateGoal = asyncHandler(async(req, res)=>{
     if(!goal){
         res.status(400)
         throw new Error('Goal not found')
+    }
+
+    const user = await User.findById(req.user.id)
+    //Check for user
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+//make sure the logged in user matches the goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
     }
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new:true})
@@ -43,6 +58,17 @@ const deleteGoal = asyncHandler(async(req, res)=>{
     if(!goal){
         res.status(400)
         throw new Error('Goal not found')
+    }
+    const user = await User.findById(req.user.id)
+    //Check for user
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+//make sure the logged in user matches the goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
     }
 
     //await goal.remove()
